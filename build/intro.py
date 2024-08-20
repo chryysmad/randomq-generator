42df6be6f981105fc5f87444bf6778b5f0ca10e1
chryysmad <chrysmad@CHRYS>
2024-08-20 14:21:12 +0300

    Feat: New structure; MCQ button loading correct page

--- build/intro.py
+++ build/intro.py
@@ -4,85 +4,69 @@
 
 
 from pathlib import Path
-
-# from tkinter import *
-# Explicit imports to satisfy Flake8
+import tkinter as tk
 from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
 
 
-OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"/home/chrysmad/randomq-generator/build/assets/frame4")
+class IntroPage(tk.Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
+        self.configure(bg="#F5F5F5")
 
+        canvas = tk.Canvas(
+            self,
+            bg="#F5F5F5",
+            height=547,
+            width=994,
+            bd=0,
+            highlightthickness=0,
+            relief="ridge"
+        )
+        canvas.place(x=0, y=0)
+        canvas.create_text(
+            24.0,
+            140.0,
+            anchor="nw",
+            text="Random Question Generator",
+            fill="#1E1E1E",
+            font=("Inter", 48 * -1)
+        )
 
-def relative_to_assets(path: str) -> Path:
-    return ASSETS_PATH / Path(path)
+        canvas.create_text(
+            24.0,
+            206.0,
+            anchor="nw",
+            text="Choose the H5P Question Type",
+            fill="#757575",
+            font=("Inter", 24 * -1)
+        )
 
+        button_image_1 = tk.PhotoImage(file=self.relative_to_assets("button_1.png"))
+        button_1 = tk.Button(
+            self,
+            image=button_image_1,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: controller.show_frame("ParametersPage"),
+            relief="flat"
+        )
+        button_1.image = button_image_1 
+        button_1.place(x=286.0, y=316.0, width=192.0, height=43.0)
 
-window = Tk()
+        button_image_2 = tk.PhotoImage(file=self.relative_to_assets("button_2.png"))
+        button_2 = tk.Button(
+            self,
+            image=button_image_2,
+            borderwidth=0,
+            highlightthickness=0,
+            command=lambda: print("button_2 clicked"),
+            relief="flat"
+        )
+        button_2.image = button_image_2
+        button_2.place(x=516.0, y=316.0, width=192.0, height=43.0)
 
-window.geometry("994x547")
-window.configure(bg = "#F5F5F5")
-
-
-canvas = Canvas(
-    window,
-    bg = "#F5F5F5",
-    height = 547,
-    width = 994,
-    bd = 0,
-    highlightthickness = 0,
-    relief = "ridge"
-)
-
-canvas.place(x = 0, y = 0)
-canvas.create_text(
-    24.0,
-    140.0,
-    anchor="nw",
-    text="Random Question Generator",
-    fill="#1E1E1E",
-    font=("Inter", 48 * -1)
-)
-
-canvas.create_text(
-    24.0,
-    206.0,
-    anchor="nw",
-    text="Choose the H5P Question Type",
-    fill="#757575",
-    font=("Inter", 24 * -1)
-)
-
-button_image_1 = PhotoImage(
-    file=relative_to_assets("button_1.png"))
-button_1 = Button(
-    image=button_image_1,
-    borderwidth=0,
-    highlightthickness=0,
-    command=lambda: print("button_1 clicked"),
-    relief="flat"
-)
-button_1.place(
-    x=286.0,
-    y=316.0,
-    width=192.0,
-    height=43.0
-)
-
-button_image_2 = PhotoImage(
-    file=relative_to_assets("button_2.png"))
-button_2 = Button(
-    image=button_image_2,
-    borderwidth=0,
-    highlightthickness=0,
-    command=lambda: print("button_2 clicked"),
-    relief="flat"
-)
-button_2.place(
-    x=516.0,
-    y=316.0,
-    width=192.0,
-    height=43.0
-)
-window.resizable(False, False)
-window.mainloop()
+    def relative_to_assets(self, path: str):
+        OUTPUT_PATH = Path(__file__).parent
+        ASSETS_PATH = OUTPUT_PATH / Path(r"/home/chrysmad/randomq-generator/build/assets/frame4")
+        return ASSETS_PATH / Path(path)
--- main.py
+++ main.py
@@ -1,9 +1,37 @@
-import subprocess
+import tkinter as tk
+from pathlib import Path
+from build.parameters import ParametersPage
+from build.intro import IntroPage
 
-gui_files = ["build/intro.py", "build/parameters.py", "build/correct.py", "build/wrongs.py", "build/randomizer.py"]
+class BaseApp(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-def launch_gui(file):
-    subprocess.Popen(["python3", file])
+        self.title("Random Question Generator")
+        self.geometry("994x547")
+        self.configure(bg="#F5F5F5")
 
-for gui in gui_files:
-    launch_gui(gui)
+        # make a container that stores all the pages
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        for F in (IntroPage, ParametersPage):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("IntroPage")
+
+    def show_frame(self, page_name):
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+if __name__ == "__main__":
+    app = BaseApp()
+    app.resizable(False, False)
+    app.mainloop()
